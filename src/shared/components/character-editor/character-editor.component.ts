7443f76f70cf95fd7621d834b7d23f23c904aa8a
harelbaruchi <harelbaruchi@gmail.com>
2024-08-18 08:10:33 +0300

    fix build target

--- src/shared/components/character-editor/character-editor.component.ts
+++ src/shared/components/character-editor/character-editor.component.ts
@@ -64,7 +64,7 @@ export class CharacterEditorComponent implements OnInit {
 
   initCharacterForm() {
     const characterDetails = this.character();
-    if (characterDetails !== null) {
+    if (characterDetails) {
       this.characterForm.patchValue({ ...characterDetails });
       this.isNewCharacter.set(false);
     } else {
